0520952f20dc4d1fd27fa7a3fcdec20ae9ba225c
Yusuf Yudhistira <yudhis96@yahoo.com>
2019-05-24 10:17:29 +0700

    commit laptop

---
+++ phpFunctionVariable.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+   /*
+      Semua fungsi dan kelas dalam PHP tidak bersifat case-sensitive 
+   */
+   ECHO "Hello World!<br>";
+   echo "Hello World!<br>";
+   EcHo "Hello World!<br><br>";
+
+   /*
+      Nama variabel dalam PHP didahului dengan tanda "$" dan bersifat case-sensitive 
+   */
+   $color = "red";
+   echo "My car is " . $color . "<br>";
+   echo "My house is " . $COLOR . "<br>";
+   echo "My boat is " . $coLOR . "<br>";
+?> 
+
+</body>
+</html>
---
+++ php_SERVER.php
@@ -0,0 +1,33 @@
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+echo $_SERVER['PHP_SELF'];
+echo "<hr>";
+echo $_SERVER['GATEWAY_INTERFACE'];
+echo "<hr>";
+echo $_SERVER['SERVER_ADDR'];
+echo "<hr>";
+echo $_SERVER['SERVER_NAME'];
+echo "<hr>";
+echo $_SERVER['SERVER_SOFTWARE'];
+echo "<hr>";
+echo $_SERVER['SERVER_PROTOCOL'];
+echo "<hr>";
+echo $_SERVER['REQUEST_METHOD'];
+echo "<hr>";
+echo $_SERVER['REQUEST_TIME'];
+echo "<hr>";
+echo $_SERVER['HTTP_HOST'];
+echo "<hr>";
+echo $_SERVER['HTTP_REFERER'];
+echo "<hr>";
+echo $_SERVER['HTTP_USER_AGENT'];
+echo "<hr>";
+echo $_SERVER['SCRIPT_NAME'];
+?>
+
+</body>
+</html>
